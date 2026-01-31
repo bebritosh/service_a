ef5c129640c0f5b7f687d4f7be92d2e208f2c916
Egor <egordorogin308@gmail.com>
2026-02-01 00:00:55 +0500

    Update Service B URL

--- bookings/views.py
+++ bookings/views.py
@@ -22,7 +22,7 @@ class CreateBookingView(APIView):
             payload = serializer.validated_data
 
             # URL Сервиса Б (проверь порт!)
-            SERVICE_B_URL = "https://service-b-j1gc.onrender.com"
+            SERVICE_B_URL = "https://service-b-j1gc.onrender.com/api/check/"
 
             try:
                 # 2. Пробуем достучаться до Сервиса Б
